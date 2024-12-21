7004a50a2bc71be12bfc0d8f1b9b9c2b29b126c1
keshew <artyom.korotckov2017@yandex.ru>
2024-12-21 16:10:14 +0500

    Initial Commit

---
+++ Wheel/WheelApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheelApp.swift
+//  Wheel
+//
+//  Created by Артём Коротков on 21.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct WheelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
